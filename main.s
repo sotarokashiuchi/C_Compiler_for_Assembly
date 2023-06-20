b517205b5c8476ce66689f1593de376a6e5e48f8
Kashiuchi Sotaro <sotaro0416@gmail.com>
2023-06-21 08:12:47 +0900

    1：整数1個をコンパイルする言語の作成 出力のみ実装

--- main.s
+++ main.s
@@ -5,61 +5,49 @@
 %endmacro
 
 [SECTION .text]
-   global _start     ;エントリーポイント
+  global _start     ;エントリーポイント
 	
 _start:
   ; 1行目
-  mov	rdx,len1     ;message length
-  mov	rsi,msg1     ;message to write
+  mov	rdx,len1
+  mov	rsi,msg1
   print
-  ; ; 1行目
-  ; mov	edx,len1     ;message length
-  ; mov	ecx,msg1     ;message to write
-  ; print
   
   ; 2行目
-  mov	rdx,len2     ;message length
-  mov	rsi,msg2     ;message to write
+  mov	rdx,len2
+  mov	rsi,msg2
   print
 
   ; 3行目
-  mov	rdx,len3     ;message length
-  mov	rsi,msg3     ;message to write
+  mov	rdx,len3
+  mov	rsi,msg3
   print
 
   ; 4行目
-  mov	rdx,len4     ;message length
-  mov	rsi,msg4     ;message to write
+  mov	rdx,len4
+  mov	rsi,msg4
   print
 
   ; 4行目
-  mov	rdx,len5     ;message length
-  mov	rsi,msg5     ;message to write
+  mov	rdx,len5
+  mov	rsi,msg5
   print
 
+  ; exit呼び出し
   mov     rax, 60
   mov     rdi, 0
   syscall
 
 [SECTION .data]
-msg1 db '[SECTION .text]', 0xa ; 「0xa」は'\n'
+msg1 db '[SECTION .text]', 0xa  ; 「0xa」は\n
 len1 equ $ - msg1
 msg2 db '  global _start', 0xa
 len2 equ $ - msg2
 msg3 db '_start:', 0xa
-len3 equ $ - msg2
-msg4 db '        mov rax, 42', 0xa
-len4 equ $ - msg3
-msg5 db '        ret', 0xa
-len5 equ $ - msg4
-
-   
-; [SECTION .data]
-; msg1 db '.intel_syntax noprefix', 0xa
-; len1 equ $ - msg1
-; msg2 db '.globl main', 0xa
-; len2 equ $ - msg2
-; msg3 db '        mov rax, 42', 0xa
-; len3 equ $ - msg3
-; msg4 db '        ret', 0xa
-; len4 equ $ - msg4
+len3 equ $ - msg3
+msg4 db '        mov ebx, 0D42', 0xa
+len4 equ $ - msg4
+msg5 db '        mov eax, 1', 0xa
+len5 equ $ - msg5
+msg6 db '        int 0x80', 0xa ;exit呼び出し
+len6 equ $ - msg6
