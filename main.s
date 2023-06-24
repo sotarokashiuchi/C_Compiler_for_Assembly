593716a9f3793878fe7228fa421d2ae87edcf2d3
Kashiuchi Sotaro <sotaro0416@gmail.com>
2023-06-24 19:43:56 +0900

    print関数作成 ヌル文字が出現するまで表示させる仕様

--- main.s
+++ main.s
@@ -1,6 +1,19 @@
-%macro print 0
-  mov rax, 1        ;writeシステムコールの識別子
-  mov rdi, 1        ;stdoutファイルディスクリプタ
+; rcxとraxの退避が必要
+%macro print 1
+  mov   rcx, 0x0
+  mov   rax, %1
+
+%%print_loop:
+  cmp   byte [rax], 0x0
+  je    %%print_done
+  inc   rax
+  inc   rcx         ; インクリメント
+  jmp   %%print_loop
+%%print_done:
+  mov rax, 1        ; write
+  mov rdi, 1        ; fd
+  mov rsi, %1       ; *buf
+  mov rdx, rcx      ; count
   syscall           ; write(fd, *buf, count)
 %endmacro
 
@@ -11,7 +24,7 @@
   global main       ;エントリーポイント
 	
 main:
-  push  rbp         ;ldリンカの場合_startがエントリーポイントのため、rbpレジスタを退避する必要はないが
+  push  rbp
   mov   rbp, rsp
 
   mov   rax, rdi    ; 第一引数(argc)
@@ -20,86 +33,41 @@ main:
   push  qword [rbx]
 
   ; 1行目
-  mov	rdx,len1
-  mov	rsi,msg1
-  print
-  mov	rdx,len2
-  mov	rsi,msg2
-  print
-  mov	rdx,len3
-  mov	rsi,msg3
-  print
-  mov	rdx,len4
-  mov	rsi,msg4
-  print
-  mov	rdx,len5
-  mov	rsi,msg5
-  print
-  mov	rdx,len6
-  mov	rsi,msg6
-  print
-
-  mov   rcx, 0x0
-  mov   rax, qword [rbp-8]
-
-.loop:
-  cmp   byte [rax], 0x0
-  je    .done
-  inc   rax
-  inc   rcx         ; インクリメント
-  jmp   .loop
-
-.done:
-  push rcx
-  mov   rsi, [rbp-8]; *buf
-  mov   rdx, rcx    ; count
-  print
-  syscall
-
-  mov	rdx,len7
-  mov	rsi,msg7
-  print
-  mov	rdx,len8
-  mov	rsi,msg8
-  print
+  print msg1
+  print msg2
+  print msg3
+  print msg4
+  print msg5
+  print msg6
+  print qword[rbp-8]
+  print msg7
+  print msg8
 
   ; 数値と文字列を変換する必要がある
-  ; mov   rsi, [rbp-16]; *buf
-  ; mov   rdx, rcx    ; count
-  ; print
-  ; syscall
+  ; print ??
 
-  mov	rdx,len9
-  mov	rsi,msg9
-  print
+  print msg9
 
   mov rsp, rbp
-  pop rbp          ;ldリンカの場合_startがエントリーポイントのため、rbpレジスタを退避する必要はないが
-  ; exit呼び出し
+  pop rbp
+
   mov rax, 60 ;exit syscall 識別子
   mov rdi, 0  ;引数1:終了ステータス
   syscall
 
 [SECTION .data]
-msg1 db '[SECTION .text]', 0xa  ; 「0xa」は\n
-len1 equ $ - msg1
-msg2 db 0x09, 'global _start', 0xa
-len2 equ $ - msg2
-msg3 db '_start:', 0xa
-len3 equ $ - msg3
-
-msg4 db 0x09, 'mov rax, 0x1', 0xa ;write syscall 識別子
-len4 equ $ - msg4
-msg5 db 0x09, 'mov rdi, 0x1', 0xa ;引数1
-len5 equ $ - msg5
-msg6 db 0x09, 'mov rsi, "'        ;引数2:*buf
-len6 equ $ - msg6
+msg1 db       '[SECTION .text]', 0xa, 0x0  ; 「0xa」は\n
+msg2 db 0x09,   'global _start', 0xa, 0x0
+msg3 db         '_start:', 0xa, 0x0
+msg4 db 0x09,   'mov rax, 0x1', 0xa, 0x0 ;write syscall 識別子
+msg5 db 0x09,   'mov rdi, 0x1', 0xa, 0x0 ;引数1
+msg6 db 0x09,   'mov rsi, "', 0x0        ;引数2:*buf
 ; 入力された文字列埋め込み
-msg7 db '"', 0xa
+msg7 db '"', 0xa, 0x0
 len7 equ $ - msg7
-msg8 db 0x09, 'mov rdx, '        ;引数3:count
+msg8 db 0x09, 'mov rdx, ', 0x0        ;引数3:count
 len8 equ $ - msg8
 ; 文字数
-msg9 db 0xa, 0x09, 'syscall', 0xa ;exit呼び出し
+msg9 db 0xa, 0x09, 'syscall', 0xa, 0x0 ;exit呼び出し
 len9 equ $ - msg9
 
