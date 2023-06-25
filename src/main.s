0e87385250f711f734a8eebffcd6e779ca6eb71f
Kashiuchi Sotaro <sotaro0416@gmail.com>
2023-06-25 18:31:21 +0900

    入力した一つの整数を文字列として受け取り、文字列として出力する

--- src/main.s
+++ src/main.s
@@ -32,21 +32,73 @@ main:
   add   rbx, 0x8    ; 第二引数(argv[1])
   push  qword [rbx]
 
-  ; 1行目
+  ; 1文字入力した文字を取得
+;   mov rax, qword[rbp-8]
+;   mov bl, byte [rax]
+
+;   cmp bl, 0x30
+;   je .strtoint_0
+;   cmp bl, 0x31
+;   je .strtoint_1
+;   cmp bl, 0x32
+;   je .strtoint_2
+;   cmp bl, 0x33
+;   je .strtoint_3
+;   cmp bl, 0x34
+;   je .strtoint_4
+;   cmp bl, 0x35
+;   je .strtoint_5
+;   cmp bl, 0x36
+;   je .strtoint_6
+;   cmp bl, 0x37
+;   je .strtoint_7
+;   cmp bl, 0x38
+;   je .strtoint_8
+;   cmp bl, 0x39
+;   je .strtoint_9
+
+; .strtoint_0:
+;   mov rbx, 0
+;   jmp .strtoint_E
+; .strtoint_1:
+;   mov rbx, 1
+;   jmp .strtoint_E
+; .strtoint_2:
+;   mov rbx, 2
+;   jmp .strtoint_E
+; .strtoint_3:
+;   mov rbx, 3
+;   jmp .strtoint_E
+; .strtoint_4:
+;   mov rbx, 4
+;   jmp .strtoint_E
+; .strtoint_5:
+;   mov rbx, 5
+;   jmp .strtoint_E
+; .strtoint_6:
+;   mov rbx, 6
+;   jmp .strtoint_E
+; .strtoint_7:
+;   mov rbx, 7
+;   jmp .strtoint_E
+; .strtoint_8:
+;   mov rbx, 8
+;   jmp .strtoint_E
+; .strtoint_9:
+;   mov rbx, 9
+;   jmp .strtoint_E
+
+; .strtoint_E:
+
+
+  ; アセンブリコード生成
   print msg1
   print msg2
   print msg3
   print msg4
   print msg5
-  print msg6
   print qword[rbp-8]
-  print msg7
-  print msg8
-
-  ; 数値と文字列を変換する必要がある
-  ; print ??
-
-  print msg9
+  print msg6
 
   mov rsp, rbp
   pop rbp
@@ -58,16 +110,16 @@ main:
 [SECTION .data]
 msg1 db       '[SECTION .text]', 0xa, 0x0  ; 「0xa」は\n
 msg2 db 0x09,   'global _start', 0xa, 0x0
-msg3 db         '_start:', 0xa, 0x0
-msg4 db 0x09,   'mov rax, 0x1', 0xa, 0x0 ;write syscall 識別子
-msg5 db 0x09,   'mov rdi, 0x1', 0xa, 0x0 ;引数1
-msg6 db 0x09,   'mov rsi, "', 0x0        ;引数2:*buf
+msg3 db         '_start:', 0xa,       0x0
+msg4 db 0x09,   'mov rax, 0x3c', 0xa, 0x0 ;exit syscall 識別子
+msg5 db 0x09,   'mov rdi, ',          0x0 ;引数1
 ; 入力された文字列埋め込み
-msg7 db '"', 0xa, 0x0
-len7 equ $ - msg7
-msg8 db 0x09, 'mov rdx, ', 0x0        ;引数3:count
-len8 equ $ - msg8
-; 文字数
-msg9 db 0xa, 0x09, 'syscall', 0xa, 0x0 ;exit呼び出し
-len9 equ $ - msg9
+msg6 db 0xa, 0x09, 'syscall', 0xa, 0x0 ;exit呼び出し
 
+; TDD手法 入力した値をexitの終了ステータスとして入れたい
+; [SECTION .text]
+; 	global _start
+; _start:
+; 	mov rax, 0x3c ;exit
+; 	mov rdi, 			;終了ステータスに入力した値を入れたい
+; 	syscall
