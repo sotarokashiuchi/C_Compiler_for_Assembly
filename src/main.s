1d2885ef3794e87695bae6c36c3a7f7d823e0c0f
Kashiuchi Sotaro <sotaro0416@gmail.com>
2023-06-28 19:38:46 +0900

    文字列から数値のみを抽出

--- src/main.s
+++ src/main.s
@@ -17,6 +17,49 @@
   syscall           ; write(fd, *buf, count)
 %endmacro
 
+; num = ( 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 )*
+; int(RAX) num(char* strar_point(RDI))
+num:
+  ; 初期設定
+  push  rbp
+  mov   rbp, rsp
+  push  rbx
+
+  mov   rax, 0x0
+  mov   rbx, 0x0
+  mov   rdx, 0x0
+
+.strtoint_loop:
+  ; 上位4bitが 0B0011 かどうか
+  mov   al, byte[rdi]
+  and   al, 0B1111_0000
+  cmp   al, 0B0011_0000
+  jnz   .strtoint_done ; ZF==0
+
+  ; 下位4bitが 0B1001 以下かどうか
+  mov   al, byte[rdi]
+  and   al, 0B0000_1111
+  mov   bl, 0B0000_1010
+  div   bl
+  cmp   al, 0x0
+  jnz   .strtoint_done ; ZF==0
+
+  mov   rax, 0x0
+  mov   al, byte [rdi]
+  and   al, 0B0000_1111   ; 文字を数値に変換
+  inc   rdi               ; rdi++
+  ; rbxレジスタを桁数に合わせて乗算する
+  imul  rdx, dword 0B1010
+  add   rdx, rax
+  jmp   .strtoint_loop
+
+.strtoint_done:
+  ; 終期設定
+  pop   rbx
+  mov   rsp, rbp
+  pop   rbp
+  ret
+
 ; 関数呼び出し規約(整数) RAX funx1(RDI, RSI, RDX, RCX, R8, R9, スタック, スタック, スタック, スタック)
 ; syscall呼び出し規約 RAX RAX(RDI, RSI, RDX, r10, r8, r9)
 
@@ -32,28 +75,13 @@ main:
   add   rbx, 0x8    ; 第二引数(argv[1])
   mov   r8, qword[rbx]
   mov   [rbp-8], r8
-  ; push  qword [rbx]
 
-  ; 十進数の文字列から二進数の数値に変換
-  mov   r8, qword[rbp-8]
-  mov   rcx, 0x0
-  mov   rax, 0x0
-  mov   rbx, 0x0
-
-.strtoint_loop:
-  cmp   byte [r8], 0x0
-  je    .strtoint_done
-  mov   bl, byte [r8]
-  and   bl, 0B0000_1111   ; 文字を数値に変換
-  inc   r8                ; r8++
-  inc   rcx               ; rcx++
-  ; rbxレジスタを桁数に合わせて乗算する
-  imul  rax, dword 0B1010
-  add   rax, rbx
-  jmp   .strtoint_loop
-.strtoint_done:
+  ; テスト実行
+  mov   rdi, qword[rbp-8]
+  call  num
 
   ; 入力数値をインクリメント
+  mov   rax, rdx
   inc   rax
 
   ; 二進数の数値を十進数の文字列に変換
